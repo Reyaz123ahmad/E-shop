91f5ebf315ef672e82ccbb6341eb2c783a920d31
Reyaz Ahmad <reyazahmad40544@gmail.com>
2026-06-16 16:40:31 +0530

    Razorpay secret problem solved

--- backend/config/razorpay.js
+++ backend/config/razorpay.js
@@ -2,6 +2,6 @@ const Razorpay = require('razorpay')
 require('dotenv').config()
 
 exports.instance = new Razorpay({
-    key_id: process.env.Razorpay_key,
-    key_secret: process.env.Razorpay_secret
+    key_id: process.env.Razorpay_KEY,
+    key_secret: process.env.Razorpay_SECRET
 })
