6c4983be0f84eafebf03474a160fa2b0645126f0
Reyaz Ahmad <reyazahmad40544@gmail.com>
2026-06-20 12:31:11 +0530

    password issue solve

--- backend/emailService/mailsender.js
+++ backend/emailService/mailsender.js
@@ -4,14 +4,11 @@ const EmailTemplate = require('../emailTemplate/template')
 require('dotenv').config()
 const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
     }
 })
 
